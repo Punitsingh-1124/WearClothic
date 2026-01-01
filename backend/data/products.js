35fd77a9bf28aa8bbe3f554f049b74bb3c83d177
Punitsingh-1124 <punitsingh2413@gmail.com>
2026-01-01 23:12:29 +0530

    Products Updates

--- backend/data/products.js
+++ backend/data/products.js
@@ -1927,7 +1927,7 @@ const products = [
     gender: "Women",
     images: [
       {
-        url: "https://m.media-amazon.com/images/I/41nT8CoQy-L.jpg",
+        url: "https://plus.unsplash.com/premium_photo-1764329344169-244b75b321ba?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         altText: "Ruffle-Sleeve Blouse",
       },
     ],
